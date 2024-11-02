12e6d49192fe07b5a493ebf8d4d1a9005bb250c2
Lenar2000 <48990912+Lenar2000@users.noreply.github.com>
2024-11-02 22:41:45 +0500

    Initial

---
+++ Controllers/MainController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SurvivalBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MainController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiBaseUrl;
+        private readonly string _apiToken;
+
+        public MainController(IConfiguration configuration)
+        {
+            _httpClient = new HttpClient();
+
+            _apiBaseUrl = "https://api.edgegap.com/v1/deployments";
+            _apiToken = "token 84baf80d-10ab-4fe6-8a5b-3733434583ae";
+
+            _httpClient.DefaultRequestHeaders.Clear();
+            _httpClient.DefaultRequestHeaders.Add("authorization", _apiToken);
+        }
+
+        [HttpGet("deployments")]
+        public async Task<IActionResult> GetDeployments()
+        {
+            var response = await _httpClient.GetAsync(_apiBaseUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return Content(content, "application/json");
+            }
+            else
+            {
+                return StatusCode((int)response.StatusCode, await response.Content.ReadAsStringAsync());
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Добавление конфигурации из appsettings.json
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
